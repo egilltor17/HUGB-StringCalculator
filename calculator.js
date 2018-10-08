e26c35ec8131e2d13ba94318db823bfc68c6a9f6
egilltor17 <egilltor17@ru.is>
2018-10-08 13:32:42 +0000

    adds zero or one number

--- calculator.js
+++ calculator.js
@@ -1,7 +1,12 @@
 // Calculator
 
-function Add(number) {
-	console.log("add function called");
+function add(number) {
+	//console.log("add function called");
+	if(number == "") {
+		return 0;
+	}
+	
+	return parseInt(number);
 }
 
 module.exports = add;
