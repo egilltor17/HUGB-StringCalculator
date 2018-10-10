77aff2a8764afc66336125bdc8308da991f1b7c1
egilltor17 <egilltor17@ru.is>
2018-10-10 14:39:05 +0000

    calculator will not except negative numbers

--- calculator.js
+++ calculator.js
@@ -1,11 +1,23 @@
 // Calculator
 
 function add(numbers) {
-	//console.log("add function called");
 	if(numbers == "") {
 		return 0;
+	} else if(numbers.includes("-")) {
+		var numberArr = numbers.split(/[\n,]+/);
+		var error = "";
+		for(var i = 0; i < numberArr.length; i++) {
+			if(numberArr[i].includes("-")) {						
+				if(error == "") {
+					error += numberArr[i];
+				} else {
+					error += (", " + numberArr[i]);
+				}
+			}
+		}
+		throw ("Negatives not allowed: " + error);
 	} else if(numbers.includes(",") || numbers.includes("\n")) {
-		var numberArr = numbers.split(/[\s,]+/);
+		var numberArr = numbers.split(/[\n,]+/);
 		var sum = 0;
 		for(var i = 0; i < numberArr.length; i++) {
 			sum += parseInt(numberArr[i]);
--- calculator.test.js
+++ calculator.test.js
@@ -12,17 +12,24 @@ it("Should return number then there is only one number in the string ", () => {
 	expect(add("5")).toBe(5);
 });
 
-// sum of two numbers
+// The sum of two numbers
 it("Should return 7 when 5 and 2 are in the string", () => {
 	expect(add("5,2")).toBe(7);
 });
 
-// sum of three numbers
+// The sum of three numbers
 it("Should return 12 when 6, 4, 2 in the string ", () => {
 	expect(add("6,4,2")).toBe(12);
 });
 
-// sum of two numbers split by \n
-it("Should return 8 when 6 \n 2 in the string ", () => {
+// The sum of two numbers split by \'n
+it("Should return 8 when 6 \'n 2 in the string ", () => {
 	expect(add("6\n2")).toBe(8);
+});
+
+// Negative exception
+it("Should throw exception when -6,  2 in the string", () => {
+	expect(() => {
+		add("-6,2")
+	}).toThrow();
 });
