284a8c317bc9f1ec43409dc1dfa4910050f1b468
egilltor17 <egilltor17@ru.is>
2018-10-11 12:54:32 +0000

    Refactor: moved the negChect into a function

--- src/calculator.js
+++ src/calculator.js
@@ -1,30 +1,15 @@
-// calculator.js
+// src/calculator.js
 
 function add(numbers) {
 	if(numbers == "") {
 		return 0;
-	} else if(numbers.includes("-")) {
-		var numberArr = numbers.split(/['\n',',']+/);
-		var error = "";
-		for(var i = 0; i < numberArr.length; i++) {
-			if(numberArr[i].includes("-")) {
-				if(error == "") {
-					error += numberArr[i];
-				} else {
-					error += (", " + numberArr[i]);
-				}
-			}
-		}
-		throw ("Negatives not allowed: " + error);
 	} else if(numbers.startsWith("//")) {
 		var nl_index = numbers.indexOf("\n");
-		var delimiter = numbers.substring(2, nl_index);
-		var foo = numbers.substring(nl_index);
-		var regexp = new RegExp("[" + delimiter + "\n,]+","g");
-		var numberArr = foo.split(regexp);
+		var del = numbers.substring(2, nl_index);
+		var numberArr = numbers.substring(nl_index).replace(del, ",").split(/[\n,]+/);
 		return sum(numberArr);
 	} else if(numbers.includes(",") || numbers.includes("\n")) {
-		var numberArr = numbers.split(/['\n',',']+/);
+		var numberArr = numbers.split(/[\n,]+/);
 		return sum(numberArr);
 	} else {
 		return parseInt(numbers);
@@ -32,6 +17,7 @@ function add(numbers) {
 }
 
 function sum(numberArr) {
+	negCheck(numberArr);
 	var sum = 0;
 	for(var i = 0; i < numberArr.length; i++) {
 		if(parseInt(numberArr[i]) <= 1000) {
@@ -41,4 +27,19 @@ function sum(numberArr) {
 	return sum;
 }
 
+function negCheck(numberArr) {
+	var error = "";
+	for(var i = 0; i < numberArr.length; i++) {	
+		if(numberArr[i].includes("-")) {
+			if(error == "") {
+				error += numberArr[i];
+			} else {
+				error += (", " + numberArr[i]);
+			}
+		}	
+	}
+	if(error != "") {
+		throw ("Negatives not allowed: " + error);
+	}
+}
 module.exports = add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,4 +1,4 @@
-// calculator.test.js
+// src/calculator.test.js
 const add = require("./calculator");
 
 // Empty string
@@ -48,14 +48,41 @@ it("Should return 35 when //; \\n 14 ; 21 in the string ", () => {
 	expect(add("//;\n14;21")).toBe(35);
 });
 
-// The sum of three numbers with a mix of custom delimiter Wasup and , \\n
+// The sum of two numbers with custom delimiter ;
+it("Should return 35 when //;..> \\n 14 ;..> 21 in the string ", () => {
+	expect(add("//;..>\n14;..>21")).toBe(35);
+});
+
+// The sum of four numbers with a mix of custom delimiter Wasup and , \\n
 it("Should return 32 when //Wasup \\n 21 Wasup 11 , 4 \\n 2 in the string ", () => {
 	expect(add("//Wasup\n21Wasup11,4\n2")).toBe(38);
 });
 
-// The sum of three numbers with a mix of custom delimiter Wasup and , \\n
+// The sum of four numbers with a mix of custom delimiter Wasup and , \\n
 it("Should throw exception when //** \\n -1 ** 5 , -8 \\n 7 in the string ", () => {
 	expect(() => {
 		add("//**\n-1**5,-8\n7")
 	}).toThrow();
+});
+
+// The sum of three numbers with a mix of custom delimiter - and , \\n
+it("Should return 38 when //- \\n 41 - 11 , 7 \\n 1 in the string ", () => {
+	expect(add("//-\n41-11,7\n1")).toBe(60);
+});
+
+// The sum of three numbers with a missing custom delimiter and , \\n
+it("Should return 18 when // \\n 12 , 4 \\n 2 in the string", () => {
+	expect(add("//\n12,4\n2")).toBe(18);
+});
+
+// The sum of three numbers with a mix of custom delimiter --- and , \\n with a negative number
+it("Should throw exception when //-+- \\n 61 -+- 14 , -41 \\n 7 in the string ", () => {
+	expect(() => {
+		add("//-+-\n61-+-14,-41\n7")
+	}).toThrow();
+});
+
+// The sum of three numbers with a mix of custom delimiter --- and , \\n with a negative number
+it("Should return 123 when //-+- \\n 61 -+- 14 , 41 \\n 7 in the string ", () => {
+	expect(add("//-+-\n61-+-14,41\n7")).toBe(123);
 });
